a26dc672046bb59c1daba9a6e4451032ae7aa999
Dhritikrishna Tripathi <dhritikrishnat@gmail.com>
2026-02-03 00:29:57 +0530

    phase 1.1 done

---
+++ src/config.py
@@ -0,0 +1,35 @@
+import os
+import tomli
+from typing import Any, Dict
+from pathlib import Path
+
+CONFIG_DIR = Path.home() / "dexidoc"
+CONFIG_FILE = CONFIG_DIR / "config.toml"
+
+def load_config() -> Dict[str, Any]:
+    """
+    Load configuration from ~/.dexidoc/config.toml
+    """
+    if not CONFIG_FILE.exists():
+        # return default config if file doesn't exist
+        return {
+            "config_path" : str(CONFIG_FILE),
+            "logging" : {
+                "level" : "INFO",
+                "file" : str(CONFIG_DIR/ "dexidoc.log")
+            }
+        }
+    
+    try:
+        with open(CONFIG_FILE, "rb") as f:
+            config = tomli.load(f)
+            config["config_path"] = str(CONFIG_FILE) # Inject path for reference
+            return config
+        
+    except Exception as e:
+        print(f"Error loading config: {e}")
+        return {}
+    
+def ensure_config_dir():
+    if not CONFIG_DIR.exists():
+        CONFIG_DIR.mkdir(parents=True, exist_ok=True)
---
+++ src/logger.py
@@ -0,0 +1,30 @@
+import logging
+import sys
+from pathlib import Path
+from typing import any, Dict
+from .config import ensure_config_dir
+
+def setup_logging(config:Dict [str, any]) -> logging.Logger:
+    """
+    Setup logging to file and stdout
+    """
+    ensure_config_dir()
+
+    log_file = config.get("logging", {}).get("file", str(Path.home()/ ".dexidoc"/ "dexidoc.log"))
+    log_level = config.get("logging {}").get("level","Info").upper()
+
+    logger = logging.getLogger("dexidoc")
+    logger.setLevel(log_level)
+
+    # File handler
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(file_handler)
+
+    # Stdout handler
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(logging.Formatter('%(levelname)s: %(message)s'))
+    logger.addHandler(stdout_handler)
+
+
+    return logger
